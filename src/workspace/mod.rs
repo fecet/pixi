14537a0c64e9d4fb98bdf4b509c661285af5fd18
fecet <xiezej@gmail.com>
2025-04-12 20:00:34 +0800

    feat: remove hash in detach env

--- src/workspace/mod.rs
+++ src/workspace/mod.rs
@@ -53,7 +53,7 @@ pub use solve_group::SolveGroup;
 use tokio::sync::Semaphore;
 use url::Url;
 pub use workspace_mut::WorkspaceMut;
-use xxhash_rust::xxh3::xxh3_64;
+// use xxhash_rust::xxh3::xxh3_64;
 
 static CUSTOM_TARGET_DIR_WARN: OnceCell<()> = OnceCell::new();
 
@@ -310,11 +310,7 @@ impl Workspace {
     /// the config
     fn detached_environments_path(&self) -> Option<PathBuf> {
         if let Ok(Some(detached_environments_path)) = self.config().detached_environments().path() {
-            Some(detached_environments_path.join(format!(
-                "{}-{}",
-                self.display_name(),
-                xxh3_64(self.root.to_string_lossy().as_bytes())
-            )))
+            Some(detached_environments_path.join(format!("{}", self.display_name())))
         } else {
             None
         }
